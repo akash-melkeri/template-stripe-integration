c91a97b9acd33047b6251846ecba384c98101b49
phantom <akashmelkeri23@gmail.com>
2023-08-14 18:28:57 +0530

    app

--- app.py
+++ app.py
@@ -1,13 +1,65 @@
 from flask import Flask
-from flask import request, jsonify
+from flask import request, jsonify, redirect
 import stripe
+import json
 app = Flask(__name__)
+from flask_cors import CORS
 
 app.config.from_pyfile('config.py')
-
+stripe.api_key = app.config['SECRET_KEY_STRIPE']
+cors = CORS(app)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+def calculate_order_amount(items):
+    return 1400
+
+@app.post("/create-checkout-session")
+def create_checkout_session():
+    post_data = request.json
+    price = post_data['price']
+    product_name = post_data['product_name']
+    try:
+        checkout_session = stripe.checkout.Session.create(
+        line_items =[{
+        'price_data' :{
+            'currency' : 'usd',  
+            'product_data': {
+                'name': product_name,
+            },
+            'unit_amount': price
+        },
+        'quantity' : 1
+        }],
+        mode= 'payment',
+        success_url= "http://localhost:9099/success",
+        cancel_url= "http://localhost:9099/cancel",
+        )
+        return redirect(checkout_session.url , code=303)
+    except Exception as e:
+        print('ok')
+        print(e)
+        return e
+
+@app.post('/create-payment-intent')
+def create_payment():
+    try:
+        data = json.loads(request.data)
+        # Create a PaymentIntent with the order amount and currency
+        intent = stripe.PaymentIntent.create(
+            amount=calculate_order_amount(data['items']),
+            currency='inr',
+            automatic_payment_methods={
+                'enabled': True,
+            },
+        )
+        print(intent['client_secret'])
+        return jsonify({
+            'clientSecret': intent['client_secret']
+        })
+    except Exception as e:
+        print("asd")
+        return jsonify(error=str(e)), 403
 
 
 @app.post("/stripe-webhook")
--- sample.config.py
+++ sample.config.py
@@ -1,1 +1,6 @@
-STRIPE_WEBHOOK_SECRET = "your stripe webhook"
+STRIPE_WEBHOOK_SECRET = "whsec_---"
+SECRET_KEY_STRIPE = "sk_test_---"
+DEBUG = True
+PRICING_PLANS = {
+    "standard_monthly":"price_---"
+}
